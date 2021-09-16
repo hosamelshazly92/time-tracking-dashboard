66525f4d4821926dd1873afbe9868f5624b9431d
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-09-17 01:14:28 +0200

    create Card component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { container } from "./App.module.css";
+import { container, section } from "./App.module.css";
 import Aside from "./components/Aside/Aside";
+import Card from "./components/Card/Card";
+import { v4 as uuid } from "uuid";
 
 function App() {
   const user = {
@@ -8,46 +10,52 @@ function App() {
       "https://res.cloudinary.com/ux-ui-designer/image/upload/v1631827404/Assets/image-jeremy_iiel4y.png",
     activities: [
       {
+        id: uuid(),
         type: "Work",
         duration: 32,
         timestamp: 36,
-        background:
-          "https://res.cloudinary.com/ux-ui-designer/image/upload/v1631828548/Assets/icon-work_qmwf2n.svg",
+        color: "orange",
+        icon: "https://res.cloudinary.com/ux-ui-designer/image/upload/v1631828548/Assets/icon-work_qmwf2n.svg",
       },
       {
+        id: uuid(),
         type: "Play",
         duration: 10,
         timestamp: 8,
-        background:
-          "https://res.cloudinary.com/ux-ui-designer/image/upload/v1631828546/Assets/icon-play_z6kaw6.svg",
+        color: "blue",
+        icon: "https://res.cloudinary.com/ux-ui-designer/image/upload/v1631828546/Assets/icon-play_z6kaw6.svg",
       },
       {
+        id: uuid(),
         type: "Study",
         duration: 4,
         timestamp: 7,
-        background:
-          "https://res.cloudinary.com/ux-ui-designer/image/upload/v1631828547/Assets/icon-study_qrcdlg.svg",
+        color: "red",
+        icon: "https://res.cloudinary.com/ux-ui-designer/image/upload/v1631828547/Assets/icon-study_qrcdlg.svg",
       },
       {
+        id: uuid(),
         type: "Exercise",
         duration: 4,
         timestamp: 5,
-        background:
-          "https://res.cloudinary.com/ux-ui-designer/image/upload/v1631828546/Assets/icon-exercise_rmxsqf.svg",
+        color: "green",
+        icon: "https://res.cloudinary.com/ux-ui-designer/image/upload/v1631828546/Assets/icon-exercise_rmxsqf.svg",
       },
       {
+        id: uuid(),
         type: "Social",
         duration: 5,
         timestamp: 10,
-        background:
-          "https://res.cloudinary.com/ux-ui-designer/image/upload/v1631828546/Assets/icon-social_hot3pz.svg",
+        color: "purple",
+        icon: "https://res.cloudinary.com/ux-ui-designer/image/upload/v1631828546/Assets/icon-social_hot3pz.svg",
       },
       {
+        id: uuid(),
         type: "Self Care",
         duration: 2,
         timestamp: 2,
-        background:
-          "https://res.cloudinary.com/ux-ui-designer/image/upload/v1631828546/Assets/icon-self-care_x1v3ut.svg",
+        color: "yellow",
+        icon: "https://res.cloudinary.com/ux-ui-designer/image/upload/v1631828546/Assets/icon-self-care_x1v3ut.svg",
       },
     ],
   };
@@ -55,6 +63,11 @@ function App() {
   return (
     <div className={container}>
       <Aside data={user} />
+      <div className={section}>
+        {user.activities.map((card) => (
+          <Card key={card.id} data={card} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,52 @@
+import {
+  container,
+  orange,
+  blue,
+  red,
+  green,
+  purple,
+  yellow,
+  top,
+  bottom,
+  sectionTop,
+  icon,
+  sectionBottom,
+  main,
+  secondary,
+} from "./Card.module.css";
+
+const Card = ({ data }) => {
+  return (
+    <div
+      className={`${container} ${data.type === "Work" ? orange : {}} ${
+        data.type === "Play" ? blue : {}
+      } ${data.type === "Study" ? red : {}} ${
+        data.type === "Exercise" ? green : {}
+      } ${data.type === "Social" ? purple : {}} ${
+        data.type === "Self Care" ? yellow : {}
+      }`}
+    >
+      <div className={top}>
+        <img src={data.icon} alt={data.type} />
+      </div>
+      <div className={bottom}>
+        <div className={sectionTop}>
+          <h4>{data.type}</h4>
+          <img
+            className={icon}
+            src="https://res.cloudinary.com/ux-ui-designer/image/upload/v1631832476/Assets/icon-ellipsis_npi85r.svg"
+            alt={data.color}
+          />
+        </div>
+        <div className={sectionBottom}>
+          <p className={main}>{data.duration}hrs</p>
+          <p className={secondary}>
+            Last Week - <strong>{data.timestamp}hrs</strong>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
